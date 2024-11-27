99271cf2ae885ee06bb32cdf1a596604ee215e50
Øyvind Hatland <oyvind.hatland@yahoo.com>
2024-11-27 11:08:06 +0100

    init

---
+++ src/main/java/no/brreg/createbranchbackend/CreateBranchBackendApplication.java
@@ -0,0 +1,13 @@
+package no.brreg.createbranchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateBranchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateBranchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/brreg/createbranchbackend/CreateBranchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.brreg.createbranchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateBranchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
