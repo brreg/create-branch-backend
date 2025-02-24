8a8b7f917d9c25a4fc903d96da94f794afd27a01
JonasT <jot@brreg.no>
2025-02-24 15:22:14 +0100

    Expanded testdata generation and options

--- src/main/java/no/brreg/createbranchbackend/controller/TestDataController.java
+++ src/main/java/no/brreg/createbranchbackend/controller/TestDataController.java
@@ -1,6 +1,5 @@
 package no.brreg.createbranchbackend.controller;
 
-
 import lombok.extern.slf4j.Slf4j;
 import no.brreg.createbranchbackend.dto.PresentationUrlDTO;
 import no.brreg.createbranchbackend.service.TestDataService;
@@ -21,26 +20,25 @@ public class TestDataController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/qrcodeNpid")
-    public ResponseEntity<?> createQrCodeNpid() {
-       try {
-            PresentationUrlDTO presentationUrlDTO = testDataService.createTestNpid();
+    public ResponseEntity<?> createQrCodeNpid(@RequestParam(value = "button", required = false, defaultValue = "no_data") String buttonContent) {
+        try {
+            PresentationUrlDTO presentationUrlDTO = testDataService.createTestNpid(buttonContent);
             return ResponseEntity.ok(presentationUrlDTO);
         } catch (Exception e) {
             log.error("Error creating QR code: {}", e.getMessage(), e);
             return ResponseEntity.status(500).body(e.getMessage());
         }
     }
+
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/qrcodeEucc")
-    public ResponseEntity<?> createQrCodeEucc() {
-
+    public ResponseEntity<?> createQrCodeEucc(@RequestParam(value = "button", required = false, defaultValue = "no_data") String buttonContent) {
         try {
-            PresentationUrlDTO presentationUrlDTO = testDataService.createTestEucc();
+            PresentationUrlDTO presentationUrlDTO = testDataService.createTestEucc(buttonContent);
             return ResponseEntity.ok(presentationUrlDTO);
         } catch (Exception e) {
             log.error("Error creating QR code: {}", e.getMessage(), e);
             return ResponseEntity.status(500).body(e.getMessage());
         }
     }
-
 }
